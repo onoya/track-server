6fc2e82e9926b112401ce780981c6a11587f9ef5
Naoya Ono <ono.naoyaa@gmail.com>
2019-08-14 02:40:57 +0800

    db setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri =
+  'mongodb+srv://admin:Testing12345@cluster0-1zo60.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', err => {
+  console.log('Error connecting to mongo instance', err);
+});
 
 app.get('/', (req, res) => {
   res.send('hi there');
