3cd35eb956a6e3309eefe52793313e8839ad890b
Naoya Ono <ono.naoyaa@gmail.com>
2019-08-14 09:40:57 +0800

    require auth middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
@@ -22,8 +23,8 @@ mongoose.connection.on('error', err => {
   console.log('Error connecting to mongo instance', err);
 });
 
-app.get('/', (req, res) => {
-  res.send('hi there');
+app.get('/', requireAuth, (req, res) => {
+  res.send(req.user.email);
 });
 
 app.listen(4555, () => console.log('Listening on port 4555'));
